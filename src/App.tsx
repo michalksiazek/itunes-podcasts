5eaff5b75dba11cb8b06ced67a7c38ce87eae8a5
michalksiazek <michal.ksiazek@icloud.com>
2021-01-22 10:50:51 +0100

    Add typescript

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+function TitleDisplay({ title }: { title: String }) {
+  return (
+    <div>
+      <b>{title}</b>
+    </div>
+  );
+}
+
+function App({ title }: { title: String }) {
+  return (
+    <div className={styles["App"]}>
+      <header className={styles["App-header"]}>
+        <img src={logo} className={styles["App-logo"]} alt="logo" />
+        <TitleDisplay title={title} />
+        <a
+          className={styles["App-link"]}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
